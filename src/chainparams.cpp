4938945f5a197e0344c79f6a6fd23d02463970ac
Coindroid <mail@coindroid.org>
2019-10-19 08:31:26 +0300

    update

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -161,7 +161,7 @@ public:
          *     CTxOut(nValue=50.00000000, scriptPubKey=0xA9037BAC7050C479B121CF)
          *   vMerkleTree: e0028e
          */
-        const char* pszTimestamp = Cannabitz Cryptocurrency 2019";
+        const char* pszTimestamp = "Cannabitz Cryptocurrency 2019";
         CMutableTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
